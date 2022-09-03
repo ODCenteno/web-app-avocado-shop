6ae25dc271c4a1545c4d3c431020b9ef8b454037
odcenteno <pixie_cemodan@hotmail.com>
2022-09-03 12:02:43 -0600

    starting app from template and fetching data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const app = document.querySelector('#app');
+const baseURL = 'https://platzi-avo.vercel.app';
+const apiURL = '/api/avo';
 
-console.log('Happy hacking :)')
+// Web API
+window.fetch(`${baseURL}${apiURL}`)
+    .then(response => response.json())
+    .then(responseJson => {
+        const responseItems = [];
+        responseJson.data.forEach( item => {
+            const title = document.createElement('h2');
+            title.textContent = item.name;
+            const image = document.createElement('img');
+            image.src = `${baseURL}${item.image}`;
+            const price = document.createElement('p')
+            price.textContent = item.price;
+            const container = document.createElement('div');
+
+            container.append(title, image, price);
+
+            responseItems.push(container);
+        })
+
+        console.log(responseItems);
+        app.append(...responseItems);
+    })
